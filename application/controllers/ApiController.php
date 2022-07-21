8a0067853ac8ce4768c67be4e95654634dbecf2d
ParkDoheum <pirbak@gmail.com>
2022-07-21 15:57:09 +0900

    0721 - 4교시

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -14,9 +14,27 @@ class ApiController extends Controller {
         return [_RESULT => $this->model->productInsert($json)];
     }
 
+    public function productList() {
+        $param = [];
+
+        if(isset($_GET["cate3"])) {
+            $cate3 = intval($_GET["cate3"]);
+            if($cate3 > 0) {
+                $param["cate3"] = $cate3;
+            }
+        } else {
+            if(isset($_GET["cate1"])) {
+                $param["cate1"] = $_GET["cate1"];
+            }
+            if(isset($_GET["cate2"])) {
+                $param["cate2"] = $_GET["cate2"];
+            }
+        }                 
+        return $this->model->productList($param);         
+    }
+
     public function productList2() {
-        $result = $this->model->productList2();
-        return $result === false ? [] : $result;
+        return $this->model->productList2();
     }
 
     public function productDetail() {
@@ -102,8 +120,7 @@ class ApiController extends Controller {
         if(count($urlPaths) !== 3) {
             exit();
         }
-        $productId = intval($urlPaths[2]);
-        
+        $productId = intval($urlPaths[2]);        
         
         try {
             $param = [
@@ -122,8 +139,32 @@ class ApiController extends Controller {
             }
         } catch(Exception $e) {            
             $this->model->rollback();
-        }    
-        
+        }
         return [_RESULT => 1];
+    }
+
+    public function cate1List() {
+        return $this->model->cate1List();
+    }
+
+    public function cate2List() {
+        $urlPaths = getUrlPaths();
+        if(count($urlPaths) !== 3) {
+            exit();
+        }        
+        $param = [ "cate1" => $urlPaths[2] ];
+        return $this->model->cate2List($param);
+    }
+
+    public function cate3List() {
+        $urlPaths = getUrlPaths();
+        if(count($urlPaths) !== 4) {
+            exit();
+        }        
+        $param = [ 
+            "cate1" => $urlPaths[2], 
+            "cate2" => $urlPaths[3]
+        ];
+        return $this->model->cate3List($param);
     }
 }
